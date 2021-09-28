e0fd143b7508c656e48fef80c1eb2dc71e690ef7
Rishabh Singh <rishimark98@gmail.com>
2021-09-28 14:52:48 +0530

    analytics

--- src/Components/Analytics/Analytics.jsx
+++ src/Components/Analytics/Analytics.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { Line } from "react-chartjs-2";
 import "./Analytics.scss";
+
+const data = {
+  labels: ["1", "2", "3", "4", "5", "6"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      fill: false,
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgba(255, 99, 132, 0.2)",
+    },
+  ],
+};
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
 
 const Analytics = () => {
   return (
@@ -16,7 +42,9 @@ const Analytics = () => {
       <div className="analytics-cards">
         <div className="analytics-cards-row">
           <div className="analytics-cards-leftCol"></div>
-          <div className="analytics-cards-rightCol"></div>
+          <div className="analytics-cards-rightCol">
+            <Line data={data} options={options} />
+          </div>
         </div>
       </div>
     </div>
